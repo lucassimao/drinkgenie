88d3216b823fc45519c5e04058b39c790e99da0c
Lucas Simão <lsimaocosta@gmail.com>
2024-11-11 00:35:54 -0300

    fix type

--- src/app/drink/[slug]/page.tsx
+++ src/app/drink/[slug]/page.tsx
@@ -5,13 +5,17 @@ import { getDrinkBySlug } from "@/lib/drinks";
 import { notFound } from "next/navigation";
 
 interface DrinkDetailProps {
-  params: ParsedUrlQuery & {
-    slug: string;
-  };
+  params: Promise<ParsedUrlQuery>;
 }
 
 export default async function DrinkDetail({ params }: DrinkDetailProps) {
-  const slug = (await params).slug;
+  const slug = (await params).slug as string;
+
+  if (!slug) {
+    notFound();
+    return;
+  }
+
   const drink = await getDrinkBySlug(slug);
 
   if (!drink) {
