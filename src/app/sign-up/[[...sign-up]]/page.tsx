c5a588c3b04f2630ea9b28f507998a4bef134b96
Lucas Simão <lsimaocosta@gmail.com>
2024-11-11 00:38:39 -0300

    wip

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,14 +1,8 @@
 import { SignUp } from "@clerk/nextjs";
-import Link from "next/link";
-import { HiArrowLongLeft } from "react-icons/hi2";
 
 export default function Page() {
   return (
     <main className="m-5">
-      <Link href="/" className="flex flex-row">
-        <HiArrowLongLeft className="mr-4" />
-        Back to home
-      </Link>
       <SignUp />
     </main>
   );
