a4f97e59e7545ab8dd6a71c5ff58cf0b6838af84
Lucas Simão <lsimaocosta@gmail.com>
2024-11-18 18:08:57 -0300

    layout

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <main className="m-5">
+    <main className="m-5 w-[768px] container mx-auto">
       <SignIn />
     </main>
   );
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <main className="m-5">
+    <main className="m-5 w-[768px] container mx-auto">
       <SignUp />
     </main>
   );
