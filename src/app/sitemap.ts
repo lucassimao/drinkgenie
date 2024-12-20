86ba85eedaf91e0b95c66e1e2615ab9f31f98387
Lucas Simão <lsimaocosta@gmail.com>
2024-12-20 10:43:51 -0300

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,15 +5,17 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const drinks = await getDrinks();
 
   const items: MetadataRoute.Sitemap = drinks.map((drink) => ({
-    url: `https://drinkgenie.app/drink/${drink.slug}`,
+    url: `https://www.drinkgenie.app/drink/${drink.slug}`,
     lastModified: drink.createdAt,
     changeFrequency: "daily",
     priority: 0.7,
-    images: drink.imageUrl ? [drink.imageUrl] : [],
+    images: [drink.imageUrl, drink.twitterSummaryLargeImage].filter(
+      (s) => typeof s == "string",
+    ),
   }));
 
   items.push({
-    url: `https://drinkgenie.app/`,
+    url: `https://www.drinkgenie.app/`,
     lastModified: new Date(),
     changeFrequency: "hourly",
     priority: 1,
