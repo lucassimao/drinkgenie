a8580b18921faa81fe041b2165834bcb169d4168
Lucas Simão <lsimaocosta@gmail.com>
2025-02-03 11:54:40 -0300

    fix stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,6 @@
 import "server-only";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-12-18.acacia",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export default stripe;
